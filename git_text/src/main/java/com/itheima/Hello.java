0308b20a08b22f5a60b60a57e8a33d0f69b9b891
admian <170737516@qq.com>
2018-12-25 17:42:31 +0800

    diyici

--- git_text/src/main/java/com/itheima/Hello.java
+++ git_text/src/main/java/com/itheima/Hello.java
@@ -3,7 +3,5 @@ package com.itheima;
 import com.sun.org.apache.xpath.internal.SourceTree;
 
 public class Hello {
-    public static void main(String[] args) {
-        System.out.println("git");
-    }
+
 }
